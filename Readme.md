fb1361f14b861688507878dc8c1e526bc7dbd826
K Harsh <harshnaidu2u@gmail.com>
2023-11-14 22:43:55 +0530

    server side GET POST

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 1. npm init for package.json
 2. npm i express@4
-3. app.js be default express code is written in app.js conventation
+3. app.js be default express code is written in app.js convention
+4. npm i nodemon
+5. package.json --> "scripts" { "start": "nodemon app.js"}
--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require('express');
+// function on calling add many methods the app function. 
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({message:'Hello from the API!!', app: 'daptours'});
+    // in the format of json on the server.
+});
+// get request on the server on this URL '/'
+
+app.post('/', (req, res) => {
+    res.send('you can post to this endpoint...')
+});
+// post request on server on same URL '/'
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App in running on the ${port}...`);
+})
+// callback function that will be called as soon as server is start listening
