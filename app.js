c46c061e8e0f541caab69c42c5e7fa68a713527e
K Harsh <harshnaidu2u@gmail.com>
2023-11-15 23:41:38 +0530

    handling POST request

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+// sets middleware 
+
 // app.get('/', (req, res) => {
 //     res.status(200).json({message:'Hello from the API!!', app: 'daptours'});
 //     // in the format of json on the server.
@@ -20,12 +23,31 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
-    })
-})
-
-app.post('/', (req, res) => {
-    res.send('you can post to this endpoint...')
+    });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    // res.send('done');
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
+
+// app.post('/', (req, res) => {
+//     res.send('you can post to this endpoint...')
+// });
 // post(create) request on server on same URL '/'
 
 const port = 3000;
