73d69efced6c839197f37f78dbd5719bef8ee49a
K Harsh <harshnaidu2u@gmail.com>
2023-11-26 10:41:50 +0530

    handling URL parameters

--- app.js
+++ app.js
@@ -16,12 +16,27 @@ app.use(express.json());
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 // route handler --> in express terms
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    console.log(req.params);
+
+    const id = req.params.id * 1;
+    // to convert any string into number multiply it with number.
+
+    const tour = tours.find(el => el.id === id);
+    // find --> array method to find data according to the ID (:id/5... or any number)
+
+    if(!tour) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        })
+    }
+
     res.status(200).json({
         status: 'success',
-        results: tours.length,
         data: {
-            tours
+            tour
         }
     });
 });
