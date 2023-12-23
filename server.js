7af3d02384cb85585b1e8976b19376738b4f1955
K Harsh <harshnaidu2u@gmail.com>
2023-12-23 22:20:37 +0530

    unhandled rejections, uncaught exceptions

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 
 const dotenv = require('dotenv');
 
+// UNCAUGHT EXCEPTIONS
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION shuting down....');
+        process.exit(1); 
+
+});
+
 dotenv.config({path: './config.env'});
 
 const app = require('./app');
@@ -23,7 +31,19 @@ mongoose.connect(DB, {
 // console.log(process.env); 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App in running on the ${port}...`);
 })
 // callback function that will be called as soon as server is start listening
+
+// Errors Outside Express: Unhandled Rejections
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION shuting down....');
+    server.close(() => {
+        process.exit(1); // 1 stand for uncaught exception
+
+        // 0 for success
+    }); // giving time to finish all the request to handled and then closed
+
+}); // for database connection error
