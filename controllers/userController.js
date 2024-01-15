32e0cf4e4f3b18044ffeb91c63dcae1bc5280334
K Harsh <harshnaidu2u@gmail.com>
2024-01-15 22:59:51 +0530

    Updating the current user: Data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,19 @@
 const User = require("../modals/userModal");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) => {
+    // ...allowedFields -- creates array containing all of the arguements that we passed in
+
+    const newObj = {};
+
+    Object.keys(obj).forEach(el => {
+        if(allowedFields.includes(el)) newObj[el] = obj[el]; // create new field in new object with same el
+
+    }); // loops through object and checks if we passed in fields
+
+    return newObj;
+};
 
 // USERS hanlders
 exports.getAllUsers = catchAsync(async(req, res, next) => {
@@ -21,6 +35,29 @@ exports.createUsers = (req, res) => {
         message: 'This route is not yet defined!'
     });
 };
+
+exports.updateMe = catchAsync(async(req, res, next) => {
+    //1. Create error if user POSTs password data
+    if(req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This route is not for password updates. Please use /updateMyPassword!!', 400));
+    };
+
+    //2. Filtered out unwanted fields names that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email'); // fields that are filtered from schema fields
+
+    //3. UPDATE user document
+    const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updateUser
+        }
+    });
+});
 
 exports.getUsers = (req, res) => {
     res.status(500).json({
