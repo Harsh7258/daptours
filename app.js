5d99c2b49e708d1e9dd22c013063e30207efd7d4
K Harsh <harshnaidu2u@gmail.com>
2024-01-19 20:10:58 +0530

    creating and getting reviews

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -80,6 +81,7 @@ app.use((req, res, next) => {
 // 3. ROUTES
 app.use('/api/v1/tours', tourRouter); // tourRouter --> middleware function
 app.use('/api/v1/users', userRouter); // userRouter --> middleware function
+app.use('/api/v1/reviews', reviewRouter); // reviewRouter --> middleware function 
 
 // HANDLING UNHANDLED ROUTES
 app.all('*', (req, res, next) => {
